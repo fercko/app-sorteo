fcbc4a264e8999ef3eae0824a77849b70dbae395
Fercko <tiriitoo1996@gmail.com>
2022-01-31 19:51:34 -0300

    some changes

---
+++ src/componentes/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { MenuTop } from './menu/MenuTop';
+import { Ruleta } from './ruleta/Ruleta';
+
+export const Main = () => {
+  return( 
+    <>
+        <MenuTop/>,
+        <Ruleta/>
+    </>
+  );
+};
---
+++ src/componentes/menu/MenuTop.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../css/menuTop.css'
+export const MenuTop = () => {
+  return (
+        <>
+            <div className='MenuTop'>
+                <ul>
+                    <li>INICIO</li>
+                    <li>RULETA</li>
+                    <li>RANDOM</li>
+                </ul>
+            </div>
+        </>
+    );
+};
